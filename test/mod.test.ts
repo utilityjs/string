163c48dcd6e872fe24e048831f7aa1876c730307
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2024-03-28 19:41:24 +0530

    fmt: formatting

--- test/mod.test.ts
+++ test/mod.test.ts
@@ -2,16 +2,9 @@ import { assertEquals, describe, it } from "../test_deps.ts";
 
 import { underscoreToCamelCase } from "../mod.ts";
 
-describe("string", function() {
-
-  it("underscoreToCamelCase()", function() {
-    assertEquals(
-      underscoreToCamelCase("hello_world"),
-      "Hello World"
-    );
-    assertEquals(
-      underscoreToCamelCase("_hello_world_"),
-      " Hello World "
-    );
+describe("string", function () {
+  it("underscoreToCamelCase()", function () {
+    assertEquals(underscoreToCamelCase("hello_world"), "Hello World");
+    assertEquals(underscoreToCamelCase("_hello_world_"), " Hello World ");
   });
 });
